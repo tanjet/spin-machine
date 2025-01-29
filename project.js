6f83fda9d950fee79784bb74edc364101073e282
Tanjet <144711080+tanjet@users.noreply.github.com>
2025-01-29 11:41:08 -0330

    uploading the initial files

---
+++ project.js
@@ -0,0 +1,122 @@
+// 1. Deposit some money
+//2. Determine number of lines to bet
+// 3. Collect a bet amount
+// 4. Roll/spin the slot machine
+// 5. Check if the user has won
+// 6. Give them the winnings
+//7. play again
+
+//function deposit(){
+    // return 1
+//}
+//another way of doing a function
+const prompt=require("prompt-sync")();
+
+// global variables 
+const ROWS = 3;
+const COLS=3;
+
+const SYMBOLS_COUNT ={
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+
+
+const SYMBOL_VALUES ={
+    "A" :5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+const deposit =() =>{
+        while(true){
+        const depositAmount =prompt("Enter a deposit amount: ")
+        //by default its a string so we have to change that to float or int
+        const numberDepositAmount= parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount)|| numberDepositAmount<=0){
+            console.log("Invalid deposit amount, try again. ");
+        }else{
+            return numberDepositAmount;
+        }
+            
+    }
+};
+
+const getNumberofLines =() =>{
+    while(true){
+        const numberOfLines =prompt("Enter the number of lines to bet on (1-3): ");
+        //by default its a string so we have to change that to float or int
+        const numberofLines= parseFloat(numberOfLines);
+
+        if (isNaN(numberOfLines)|| numberOfLines<=0 || numberOfLines >3){
+            console.log("Invalid dnumber of lines, try again. ");
+        }else{
+            return numberOfLines;
+        }
+            
+    }
+
+}
+// collecting a bet amount
+const getBet =(balance, lines) =>{
+    while(true){
+        const bet =prompt("Enter the bet per line: ")
+        //by default its a string so we have to change that to float or int
+        const numberBet= parseFloat(bet);
+
+        if (isNaN(numberBet)|| numberBet<=0 || numberBet> balance/lines){
+            console.log("Invalid bet amount, try again. ");
+        }else{
+            return numberBet;
+        }
+            
+    }
+};
+// spin the slot machine
+
+const spin =() =>{
+    const symbols =[];
+    for (const[symbol,count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i=0;i<count;i++){
+            symbols.push(symbol); // push pushes elements into array
+
+        }
+    }
+    const reels=[];
+    for(let i=0;i<COLS;i++){
+        reels.push([])
+        const reelSymbols=[...symbols];//symbols will be called into the array
+        
+        for(let j=0; j<ROWS; j++){
+            const randomIndex=Math.floor(Math.random()* reelSymbols.length);
+           const selectedSymbol= reelSymbols[randomIndex];
+           reels[i].push(selectedSymbol);
+           reelSymbols.splice(randomIndex,1)//splice removes the element
+        }
+
+    }
+    return reels;
+};
+
+const transpose=(reels) =>{
+    const rows=[];
+    for(let i=0;i<ROWS; i++){
+        rows.push([]);
+        for (let j=0; j<COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+    return rows
+}
+
+let balance = deposit(); // let lets change the variables
+const numberOfLines=getNumberofLines(); //constant does not let change the variables later on
+const bet=getBet(balance,numberOfLines);
+const reels=spin();
+const rows=transpose(reels)
+console.log(reels);
+console.log(rows);
